75d6ae174b4242696a326bb422aa3744db830f30
Santiago M <smquintero99@gmail.com>
2020-03-17 13:20:55 -0700

    Finalized Static Landing Site.

--- front/admin/src/components/layout.js
+++ front/admin/src/components/layout.js
@@ -34,10 +34,10 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer style={{height:60}}>
+          <span style={{display:'none'}}>
+            © {new Date().getFullYear()}, Built with {` `} <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </span>
         </footer>
       </div>
     </>
--- front/admin/src/pages/index.js
+++ front/admin/src/pages/index.js
@@ -2,9 +2,9 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 import "./style.scss"
+
 
 const cards = [
   { title: 'ONGs', content: 0, align: 'centered' },
@@ -12,13 +12,15 @@ const cards = [
   { title: 'Recently Updated', content: 0, align: 'centered' }
 ]
 
+
 const ongs = [
-  { name: 'Org 1', email: 'mail@org1.com', phone: '55-1111-1111', description: true, images: 1 },
-  { name: 'Org 2', email: 'mail@org2.com', phone: '55-1111-1111', description: true, images: 2 },
-  { name: 'Org 3', email: 'mail@org3.com', phone: '55-1111-1111', description: true, images: 3 },
-  { name: 'Org 4', email: 'mail@org4.com', phone: '55-1111-1111', description: true, images: 4 },
-  { name: 'Org 5', email: 'mail@org5.com', phone: '55-1111-1111', description: true, images: 5 },
+  { id: 1, name: 'Org 1', email: 'mail@org1.com', phone: '55-1111-1111', description: true, images: 1 },
+  { id: 2, name: 'Org 2', email: 'mail@org2.com', phone: '55-2222-2222', description: false, images: 2 },
+  { id: 3, name: 'Org 3', email: 'mail@org3.com', phone: '55-3333-3333', description: true, images: 3 },
+  { id: 4, name: 'Org 4', email: 'mail@org4.com', phone: '55-4444-4444', description: false, images: 4 },
+  { id: 5, name: 'Org 5', email: 'mail@org5.com', phone: '55-5555-5555', description: true, images: 5 },
 ]
+
 
 
 const IndexPage = () => (
@@ -26,7 +28,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <nav className="breadcrumb" aria-label="breadcrumbs">
       <ul>
-        <li><Link to="#">Home</Link></li>
+        <li><Link to="/">Home</Link></li>
       </ul>
     </nav>
 
@@ -55,7 +57,8 @@ const IndexPage = () => (
     </div>
 
 
-    <table class="table" style={{ marginTop: '4rem'}}>
+    <p className={`title is-4`} style={{color:'rebeccapurple', marginTop: '4rem'}}> List of ONGs </p> 
+    <table className="table has-text-centered">
       <thead>
         <tr>
           <th><abbr title="Index">#</abbr></th>
@@ -69,70 +72,18 @@ const IndexPage = () => (
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th>1</th>
-          <td><Link to="/page-2/" title="Leicester City">Leicester City</Link> <strong>(C)</strong></td>
-          <td>38</td><td>23</td><td>12</td><td>3</td>
-          <td><Link to="/edit" title="Edit">Edit</Link></td>
-          <td><Link to="/delete" title="Delete" className="has-text-danger">Delete</Link></td>
-        </tr>
-
-        <tr>
-          <th>2</th>
-          <td><Link to="/page-2/" title="Arsenal">Arsenal</Link></td>
-          <td>38</td><td>20</td><td>11</td><td>7</td>
-          <td><Link to="/edit" title="Edit">Edit</Link></td>
-          <td><Link to="/delete" title="Delete" className="has-text-danger">Delete</Link></td>
-        </tr>
-
-        <tr>
-          <th>3</th>
-          <td><Link to="/page-2/" title="Tottenham Hotspur">Tottenham Hotspur</Link></td>
-          <td>38</td><td>19</td><td>13</td><td>6</td>
-          <td><Link to="/edit" title="Edit">Edit</Link></td>
-          <td><Link to="/delete" title="Delete" className="has-text-danger">Delete</Link></td>
-        </tr>
-
-        <tr class="is-selected">
-          <th>4</th>
-          <td><Link to="/page-2/" title="Manchester City">Manchester City</Link></td>
-          <td>38</td><td>19</td><td>9</td><td>10</td>
-          <td><Link to="/edit" title="Edit">Edit</Link></td>
-          <td><Link to="/delete" title="Delete" className="has-text-danger">Delete</Link></td>
-        </tr>
-
-        <tr>
-          <th>5</th>
-          <td><Link to="/page-2/" title="Manchester United">Manchester United</Link></td>
-          <td>38</td><td>19</td><td>9</td><td>10</td>
-          <td><Link to="/edit" title="Edit">Edit</Link></td>
-          <td><Link to="/delete" title="Delete" className="has-text-danger">Delete</Link></td>
-        </tr>
+        { ongs.map(({ id, name, email, phone, description, images }) => <tr>
+          <th> {id} </th>
+          <td><Link to={`/org/${id}`} title="Leicester City"><strong> { name } </strong></Link></td>
+          <td> { email } </td>
+          <td> { phone } </td>
+          <td className="is-capitalized"> { String(description) } </td>
+          <td> { images } </td>
+          <td><Link to={`/edit/${id}`} title="Edit">Edit</Link></td>
+          <td><Link to={`/delete/${id}`} title="Delete" className="has-text-danger">Delete</Link></td>
+        </tr> )}
       </tbody>
     </table>
-
-    <br/><br/>
-
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-  
-    <div className="container">
-      <div className="columns">
-        <div className="column">
-          <h2 className="title is-2">Level 2 heading</h2>
-          <p className="content">Cool content. Using Bulma!</p>
-        </div>
-        <div className="column is-four-fifths">
-          <h2 className="title is-2">Level 2 heading</h2>
-          <p className="content">This column is cool too!</p>
-        </div>
-      </div>
-    </div>    
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
